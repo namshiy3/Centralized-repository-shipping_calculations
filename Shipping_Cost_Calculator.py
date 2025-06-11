5daadc5737ec9607f73dc710835919cbe2ccf8a0
Mohammed Namshiy <mohammednamshiy@Namshiys-Macbook-Pro.local>
2025-06-11 10:04:50 +0530

    Added Currency Formatting

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,12 +1,11 @@
 # Shipping Cost Calculator
 
-## Input package weight and shipping rate
+# Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
 
-## Calculate shipping cost
+# Calculate shipping cost
 shipping_cost = weight * rate
 
-## Display the result
-print(f"Shipping Cost: {shipping_cost} USD")
-
+# Display the result with 2 decimal places
+print(f"Shipping Cost: ${shipping_cost:.2f} USD")
